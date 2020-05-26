57642b1c7191e1b41f917421af1318df0bd88e92
Udaydsmls <60814531+Udaydsmls@users.noreply.github.com>
2020-05-26 16:13:19 +0530

    Update flappy.py

--- flappy.py
+++ flappy.py
@@ -8,12 +8,12 @@ WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
 BIRD_IMGS = [
-    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "D:/Uday/imgs/bird1.png"))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "D:/Uday/imgs/bird2.png"))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "D:/Uday/imgs/bird3.png")))]
-PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "D:/Uday/imgs/pipe.png")))
-BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "D:/Uday/imgs/base.png")))
-BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "D:/Uday/imgs/bg.png")))
+    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
 
 STAT_FONT = pygame.font.SysFont("comicsans", 50)
 
